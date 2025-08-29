f90a28090570da077a16895b628ecbb8d6a2071a
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2025-08-29 15:04:11 +0700

    добавлены модели пользователя, камер, изображения и галереи

---
+++ Users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from tg_camera.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(unique=True, verbose_name='почта', **NULLABLE)
+
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
--- tg_camera/models.py
+++ tg_camera/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from Users.models import User
+
+NULLABLE = {"blank": True, "null": True}
+
+class Camera(models.Model):
+    name = models.CharField(max_length=64, verbose_name="название камеры")
+    camera_url = models.URLField(**NULLABLE, verbose_name="ссылка на камеру")
+
+    class Meta:
+        verbose_name = "камера"
+        verbose_name_plural = "камеры"
+
+
+class Gallery(models.Model):
+    name = models.CharField(max_length=32, verbose_name="имя галереи")
+    owner = models.ForeignKey(User, )
+
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=128, verbose_name="имя рисунка")
+    description = models.CharField(**NULLABLE, max_length=1024, verbose_name="описание")
+    image = models.ImageField(upload_to="media/", **NULLABLE, verbose_name="изображение")
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE, verbose_name="галерея к которой принадлежит изображение")
