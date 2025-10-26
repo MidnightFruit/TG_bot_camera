d3707292b2c9d2d71dd258460bf108f0dd5cd880
dmitriy <dima.gladkikh.2016@mail.ru>
2025-10-26 16:17:24 +0700

    Переработана структура проекта, обновлён gitignore, добавлены миграции

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tg_camera',
+    'camera',
+    'gallery',
     'Users',
 ]
 
@@ -138,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'Users.User'
